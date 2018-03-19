79f5da75161f93579e7f524111af4c7f9644eaa6
billy <buanyz03@gmail.com>
2018-03-19 18:35:20 +0800

    a044
    https://zerojudge.tw/ShowProblem?problemid=a044

---
+++ a044.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;                 
+int main ()         
+{         
+    int n;         
+    while (cin>>n)         
+    { 
+	   cout<<(n*n*n-n)/6+n+1<<endl;
+	}
+}
